22c5b16b99d3217e2f144658f51b49419487d368
Axel Gil <73158076+AxelGil@users.noreply.github.com>
2024-03-25 14:41:04 +0100

    Create user and job tables

--- migrate.js
+++ migrate.js
@@ -2,17 +2,33 @@ const knex = require('knex')(require('./knexfile')['development']);
 
 async function createTable() {
   try {
-    const exists = await knex.schema.hasTable('boissons');
-    if (!exists) {
-      await knex.schema.createTable('boissons', table => {
+    const userExists = await knex.schema.hasTable('user');
+    if (!userExists) {
+      await knex.schema.createTable('user', table => {
         table.increments('id').primary();
         table.string('name');
-        table.integer('price');
-        table.integer('quantity');
+        table.string('email');
+        table.string('password');
       });
-      console.log('La table "boissons" a été créée avec succès.');
+      console.log('La table "user" a été créée avec succès.');
     } else {
-      console.log('La table "boissons" existe déjà.');
+      console.log('La table "user" existe déjà.');
+    }
+
+    const jobExists = await knex.schema.hasTable('job');
+    if (!jobExists) {
+      await knex.schema.createTable('job', table => {
+        table.increments('id').primary();
+        table.string('titre_poste');
+        table.string('job_type');
+        table.string('companie');
+        table.string('location');
+        table.string('salaire');
+        table.integer('nb_postulant');
+      });
+      console.log('La table "job" a été créée avec succès.');
+    } else {
+      console.log('La table "job" existe déjà.');
     }
   } catch (error) {
     console.error('Erreur lors de la création de la table :', error);
